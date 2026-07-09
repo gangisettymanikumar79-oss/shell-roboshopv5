fe2e32e875b4566b8cabae6cc399b5aec618d3bd
.email.com <gangisettymani98@gmail.com>
2026-07-09 17:43:07 +0530

    if i want create updateing file

---
+++ 02-roboshop-redis.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source ./common.sh
+
+check_root
+
+dnf module disable redis -y
+dnf module enable redis:7 -y
+
+dnf install redis -y 
+validate $? "installing redis :7"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/c protected-mode no' /etc/redis/redis.conf    
+validate $? "Allowing remote connections"
+
+systemctl enable redis 
+systemctl start redis 
+validate $? "starting redius"
+
+
