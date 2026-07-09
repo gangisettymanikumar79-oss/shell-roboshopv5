ede63a36ba73d6b0067567e211cc4305ac0e7c90
.email.com <gangisettymani98@gmail.com>
2026-07-09 19:44:52 +0530

    if i want create updateing file

--- common.sh
+++ common.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 LOGS_FOLDER="/var/log/roboshop"
-sudo mkdir -p $LoGS_FOLDER
+sudo mkdir -p $LOGS_FOLDER
 sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
 sudo chmod -R 755 $LOGS_FOLDER
 LOGS_FILE="$LOGS_FOLDER/$0.log"
