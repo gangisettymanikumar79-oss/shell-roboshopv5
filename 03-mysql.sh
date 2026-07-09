62eb4840c7ce0f2752c75715d655ad1430f91313
.email.com <gangisettymani98@gmail.com>
2026-07-09 17:49:22 +0530

    if i want create updateing file

---
+++ 03-mysql.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source ./common.sh
+
+check_root
+
+dnf install mysql-server -y
+validate $? "installing mysql-server"
+
+systemctl enable mysqld
+systemctl start mysqld  
+ validate $? "enable and start mysqld"
+mysql_secure_installation --set-root-pass RoboShop@1
+validate $? "setting up root password"
