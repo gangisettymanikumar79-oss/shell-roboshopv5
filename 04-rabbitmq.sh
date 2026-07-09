df2a451a4bd868e9a97c0f39400ff3880b068887
.email.com <gangisettymani98@gmail.com>
2026-07-09 17:57:18 +0530

    if i want create updateing file

---
+++ 04-rabbitmq.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source ./common.sh
+check_root
+
+cp RabbitMQ.repo /etc/yum.repos.d/RabbitMQ.repo
+validate $? "Adding RabbitMQ.repo"
+
+dnf install rabbitmq-server -y
+validate $? "installing rabbitmq-server "
+
+systemctl enable rabbitmq-server
+systemctl start rabbitmq-server
+
+validate $? "enanle and start rabbitmq-server "
+rabbitmqctl add_user roboshop roboshop123
+
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+validate $? "setting up username and password"
