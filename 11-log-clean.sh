a5db0c2ff432d7f9ad17c25aeefa3ec28a632fb6
.email.com <gangisettymani98@gmail.com>
2026-07-10 15:14:30 +0530

    if i want create the file

--- 11-log-clean.sh
+++ 11-log-clean.sh
@@ -5,7 +5,7 @@ days={3:-14}
 
 if [ -z "$source_DIR" ]; then
     echo "missing parameter"
-    echo "Usage: $0 <source_dir> [days :-14]" #default value the code
+    echo "Usage: $0 <source_dir> [days(optional default to 14]" #default value the code
     exit 1
 fi
 if [ ! -d "$source_DIR" ]; then
