4cab7291aef70c4202de46749c372bcc781139ba
.email.com <gangisettymani98@gmail.com>
2026-07-10 15:01:59 +0530

    if i want create the file

---
+++ 01-log-clean.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+$source_DIR=$1
+days={3:-14}
+
+if [ -z "$source_DIR" ]; then
+    echo "missing parameter"
+    echo "Usage: $0 <source_dir> [days :-14]" #default value the code
+    exit 1
+fi
+if [ ! -d "$source_DIR" ]; then
+    echo "ERROR : Directory:$source_DIR doesnot exit code"
+    exit 1
+fi
+
+
