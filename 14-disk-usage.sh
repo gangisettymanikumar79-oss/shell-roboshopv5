d33058991ca9f54a22701945507429c477015d2b
.email.com <gangisettymani98@gmail.com>
2026-07-14 16:39:16 +0530

    if want create new file.so we don't open file

--- 14-disk-usage.sh
+++ 14-disk-usage.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-Disk_USAGE=$(df -hT | grep -v Filesystem)
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -v Filesystem)
 USAGE_THRESHOLD=10
 
-while IFS= read -r line 
+SERVER_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
+while IFS= read -r line
 do
-   USAGE=$(echo "$line" | awk '{print $5}' | cut -d "%" -f1)
-   PARTITION=$( echo $line | awk '{print $7}' )
+    USAGE=$(echo "$line" | awk '{print $5}' | cut -d "%" -f1)
+    PARTITION=$(echo "$line" | awk '{print $7}')
 
- if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
+    if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
         MESSAGE+="High Disk Usage on $PARTITION: ${USAGE}%\n"
-   fi
-done <<< "$Disk_USAGE"
+    fi
+done <<< "$DISK_USAGE"
 
-echo -e "$Message"
+echo -e "$MESSAGE"
+
+sh mail.sh "DevOps Team" "High Disk usage" "$SERVER_IP" "$MESSAGE" "gangisettymanikumar16@gmail.com" "High Disk Usage Alert"
 
 
