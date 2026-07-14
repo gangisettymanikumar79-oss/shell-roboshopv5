4e7fb8f9239b3f17e0805301daaec8a13d887fa9
.email.com <gangisettymani98@gmail.com>
2026-07-14 09:46:22 +0530

    if want create new file.so we don't open file

--- 13-log-archieve.sh
+++ 13-log-archieve.sh
@@ -9,13 +9,19 @@ if [ -z "source_dir" ] || if [ -z "dest_dir" ]; then
    exit 1
 fi
 # Check source directory exists
+
 if [ ! -d "source_dir" ]; then
   echo "source directory : $source_dir doesn't is empty"
   exit 1
 fi
 # Check destination directory exists
+
 if [ ! -d "dest_dir" ]; then
   echo "dest_dir : $dest_dir doesn't is empty"
   exit 1
 fi
+
+echo "Source Directory      : $source_dir"
+echo "Destination Directory : $destination_dir"
+echo "Days                  : $days"
 
