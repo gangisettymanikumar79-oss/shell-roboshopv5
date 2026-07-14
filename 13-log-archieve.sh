7cf983859c6f74be66a240a9161592232c10702a
.email.com <gangisettymani98@gmail.com>
2026-07-14 09:39:55 +0530

    if want create new file.so we don't open file

--- 13-log-archieve.sh
+++ 13-log-archieve.sh
@@ -9,12 +9,12 @@ if [ -z "source_dir" ] || if [ -z "destion_dir" ]; then
    exit 1
 fi
 
-if [ -d "source_dir" ]; then
+if [ ! -d "source_dir" ]; then
   echo "source directory : $source_dir doesn't is empty"
   exit 1
 fi
 
-if [ -d "destion_dir" ]; then
+if [ ! -d "destion_dir" ]; then
   echo "destion_dir : $destion_dir doesn't is empty"
   exit 1
 fi
