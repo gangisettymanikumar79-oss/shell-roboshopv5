4c964ef7ff59164616ea88c54f8ff8fec3e12a11
.email.com <gangisettymani98@gmail.com>
2026-07-14 09:57:38 +0530

    if want create new file.so we don't open file

--- 13-log-archieve.sh
+++ 13-log-archieve.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
 source_dir=$1
-dest_dir=$2
+destination_dir=$2
 days=${3:-14}
-if [ -z "source_dir" ] || if [ -z "dest_dir" ]; then
-   echo "ether sourece directory or destination directory or empty"
-   echo "usage: $0 [source_dir] [dest_dir] [days:default 14]"
-   exit 1
+
+# Check arguments
+if [ -z "$source_dir" ] || [ -z "$destination_dir" ]; then
+    echo "Either source directory or destination directory is missing."
+    echo "Usage: $0 <source_dir> <destination_dir> [days (default: 14)]"
+    exit 1
 fi
+
 # Check source directory exists
-
-if [ ! -d "source_dir" ]; then
-  echo "source directory : $source_dir doesn't is empty"
-  exit 1
+if [ ! -d "$source_dir" ]; then
+    echo "Source directory '$source_dir' does not exist."
+    exit 1
 fi
-# Check destination directory exists
 
-if [ ! -d "dest_dir" ]; then
-  echo "dest_dir : $dest_dir doesn't is empty"
-  exit 1
+# Check destination directory exists
+if [ ! -d "$destination_dir" ]; then
+    echo "Destination directory '$destination_dir' does not exist."
+    exit 1
 fi
 
 echo "Source Directory      : $source_dir"
