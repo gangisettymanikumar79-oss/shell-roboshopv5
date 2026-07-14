68dd1b401b21ae60c999711ba85c49afa5a7eb5b
.email.com <gangisettymani98@gmail.com>
2026-07-14 09:42:29 +0530

    if want create new file.so we don't open file

--- 13-log-archieve.sh
+++ 13-log-archieve.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 source_dir=$1
-destion_dir=$2
+dest_dir=$2
 Days={3:-14}
-if [ -z "source_dir" ] || if [ -z "destion_dir" ]; then
-   echo "ether sourece directory or destion directory or empty"
+if [ -z "source_dir" ] || if [ -z "dest_dir" ]; then
+   echo "ether sourece directory or destination directory or empty"
    echo "usage: $0 [source_dir] [dest_dir] [days:default 14]"
    exit 1
 fi
@@ -14,8 +14,8 @@ if [ ! -d "source_dir" ]; then
   exit 1
 fi
 
-if [ ! -d "destion_dir" ]; then
-  echo "destion_dir : $destion_dir doesn't is empty"
+if [ ! -d "dest_dir" ]; then
+  echo "dest_dir : $dest_dir doesn't is empty"
   exit 1
 fi
 
