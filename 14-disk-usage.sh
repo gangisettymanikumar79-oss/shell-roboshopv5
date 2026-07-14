190d3996f46e90229d377b78374b762bb6b735ac
.email.com <gangisettymani98@gmail.com>
2026-07-14 13:03:54 +0530

    if want create new file.so we don't open file

--- 14-disk-usage.sh
+++ 14-disk-usage.sh
@@ -8,8 +8,9 @@ while IFS= read -r line
 do
    USAGE=$(echo "$line" | awk '{print $5}' | cut -d "%" -f1)
    PARTITION=$( echo $line | awk '{print $7}' )
-if [ "$USAGE" -ge "$USAGE_THRESHOLD"]; then
-   MESSAGE+="High Disk Usage on $PARTITION: $USAGE \n"
+
+ if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
+        MESSAGE+="High Disk Usage on $PARTITION: ${USAGE}%\n"
    fi
 done <<< "$Disk_USAGE"
 
