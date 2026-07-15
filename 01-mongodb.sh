c70d08ece235aab182a58c5ecdd81fa662bf44a5
.email.com <gangisettymani98@gmail.com>
2026-07-15 16:36:48 +0530

    if i want create new file

--- 01-mongodb.sh
+++ 01-mongodb.sh
@@ -5,19 +5,20 @@ check_root
 
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-validate $? "Adding mongo.repo"
+VALIDATE $?  "Adding mongo.repo"
 
 dnf install mongodb-org -y &>>$LOGS_FILE
-validate $? "Installing MongoDB"
+VALIDATE $? "Installing MongoDB"
 
 systemctl enable --now mongod
-validate $? "Starting and enabling MongoDB"
+VALIDATE $? "Starting and enabling MongoDB"
 
 sed -i s/127.0.0.1/0.0.0.0/g /etc/mongod.conf
-validate $? "Allowing remote connections to MongoDB"
+VALIDATE $? "Allowing remote connections to MongoDB"
 
 systemctl restart --now mongod
-validate $? " Restarting MongoDB"
+VALIDATE $? " Restarting MongoDB"
+
 
 print_total_time
 
