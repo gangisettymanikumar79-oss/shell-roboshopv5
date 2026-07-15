7b5b2b41a0aac115755ebe08e34cedee44500b50
.email.com <gangisettymani98@gmail.com>
2026-07-15 16:38:53 +0530

    if i want create new file

--- 02-roboshop-redis.sh
+++ 02-roboshop-redis.sh
@@ -8,14 +8,14 @@ dnf module disable redis -y &>>$LOGS_FILE
 dnf module enable redis:7 -y &>>$LOGS_FILE
 
 dnf install redis -y &>>$LOGS_FILE
-validate $? "installing redis :7"
+VALIDATE $? "installing redis :7"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/c protected-mode no' /etc/redis/redis.conf    
-validate $? "Allowing remote connections"
+VALIDATE $? "Allowing remote connections"
 
 systemctl enable redis &>>$LOGS_FILE
 systemctl start redis &>>$LOGS_FILE
-validate $? "starting redius"
+VALIDATE $? "starting redius"
 print_total_time
 
 
--- 03-mysql.sh
+++ 03-mysql.sh
@@ -3,13 +3,13 @@ source ./common.sh
 check_root
 
 dnf install mysql-server -y  &>> $LOGS_FILE
-validate $? "installing mysql-server"
+VALIDATE $? "installing mysql-server"
 
 systemctl enable mysqld  &>> $LOGS_FILE
 systemctl start mysqld   &>> $LOGS_FILE
- validate $? "enable and start mysqld"
+VALIDATE $? "enable and start mysqld"
 mysql_secure_installation --set-root-pass RoboShop@1
-validate $? "setting up root password"
+VALIDATE $? "setting up root password"
 
 print_total_time
 
